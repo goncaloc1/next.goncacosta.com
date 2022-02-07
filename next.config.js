b409b259f0d1fd1db3a749c45f6b1ec7552cdf7c
goncalo <goncalo.costa@agos-tech.com>
2022-02-07 10:24:27 +0000

    chore: move CSP headers from page markup to server
    There's an overhead when injecting CSP headers into every
    HTTP response but that's acceptable.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,36 @@
+const ContentSecurityPolicy = `
+  default-src 'none';
+  script-src 'self' 'unsafe-eval';
+  connect-src 'self' https://vitals.vercel-insights.com/v1/vitals;
+  prefetch-src 'self';
+  style-src 'self' 'unsafe-inline';
+  font-src data:;
+  img-src 'self' data:;
+  upgrade-insecure-requests;
+  block-all-mixed-content;
+  frame-ancestors 'none';
+  form-action 'none';
+`;
+
+const securityHeaders = [
+  {
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()
+  }
+];
+
 module.exports = {
   eslint: {
     dirs: ['pages', 'components', 'lib', 'hooks']
   },
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in the application.
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ]
+  },
 }

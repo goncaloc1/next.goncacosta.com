c53d60cbb6d4c64dd16306eff7506c2c86668888
goncaloc1 <goncacosta@gmail.com>
2024-12-02 11:12:25 +0000

    refactor: simplifying Alba text styles

--- pages/alba/index.tsx
+++ pages/alba/index.tsx
@@ -47,136 +47,124 @@ const Alba: NextPageWithLayout = () => {
           <i>Jorge Luis Borges</i>
         </div>
         <div className={styles.title}>
-          <div className="title_left">Alba</div>
-          <div className="title_right">2011</div>
+          <div className={styles.title_left}>Alba</div>
+          <div className={styles.title_right}>2011</div>
         </div>
       </Box>
       <Box photoSrc={photoa} photoPriority={true} />
       <Box photoSrc={photob} />
       <Box photoSrc={photoc} />
       <Box photoSrc={photod} />
-      <Box className="box">
-        <div className={styles.box_text_wrapper_1}>
-          <div className={styles.box_text}>
-            "(...)You know this used to be a hell of a good country.
-            <br />
-            I can't understand what's gone wrong with it.
-            <br />
-            <br />
-            Everybody got chicken, that's what happened.
-            <br />
-            Hey, we can't even get into a second-rate hotel.
-            <br />
-            I mean, a second-rate motel, you dig?
-            <br />
-            They think we'd cut their throat, man.
-            <br />
-            They're scared, man.
-            <br />
-            <br />
-            They're not scared of you.
-            <br />
-            They're scared of what you represent to them.
-            <br />
-            <br />
-            Hey man, all we represent to them is somebody who needs a haircut.
-            <br />
-            <br />
-            Oh, no.
-            <br />
-            What you represent to them is freedom.
-            <br />
-            <br />
-            What the hell's wrong with freedom? That's what it's all about.
-            <br />
-            <br />
-            Oh yeah, that's right.
-            <br />
-            That's what it's all about allright.
-            <br />
-            But talking about it and being it that's two different things.
-            <br />
-            I mean, it's real hard to be free when you are bought and sold in
-            the marketplace.
-            <br />
-            Don't tell anybody that they're not free, because they'll get busy
-            killing and maiming to prove to you that they are.
-            <br />
-            Oh yeah they're going to talk to you and talk to you and talk to you
-            about individual freedom. <br />
-            But they see a free individual, it's going to scare them.
-            <br />
-            <br />
-            Well man, it don't make them running scared.
-            <br />
-            <br />
-            No, it makes them dangerous(...)"
-            <br />
-            <br />
-            <i>Easy Rider, Dennis Hopper</i>
-          </div>
-        </div>
+      <Box className={`box ${styles.box_text}`}>
+        "(...)You know this used to be a hell of a good country.
+        <br />
+        I can't understand what's gone wrong with it.
+        <br />
+        <br />
+        Everybody got chicken, that's what happened.
+        <br />
+        Hey, we can't even get into a second-rate hotel.
+        <br />
+        I mean, a second-rate motel, you dig?
+        <br />
+        They think we'd cut their throat, man.
+        <br />
+        They're scared, man.
+        <br />
+        <br />
+        They're not scared of you.
+        <br />
+        They're scared of what you represent to them.
+        <br />
+        <br />
+        Hey man, all we represent to them is somebody who needs a haircut.
+        <br />
+        <br />
+        Oh, no.
+        <br />
+        What you represent to them is freedom.
+        <br />
+        <br />
+        What the hell's wrong with freedom? That's what it's all about.
+        <br />
+        <br />
+        Oh yeah, that's right.
+        <br />
+        That's what it's all about allright.
+        <br />
+        But talking about it and being it that's two different things.
+        <br />
+        I mean, it's real hard to be free when you are bought and sold in the
+        marketplace.
+        <br />
+        Don't tell anybody that they're not free, because they'll get busy
+        killing and maiming to prove to you that they are.
+        <br />
+        Oh yeah they're going to talk to you and talk to you and talk to you
+        about individual freedom. <br />
+        But they see a free individual, it's going to scare them.
+        <br />
+        <br />
+        Well man, it don't make them running scared.
+        <br />
+        <br />
+        No, it makes them dangerous(...)"
+        <br />
+        <br />
+        <i>Easy Rider, Dennis Hopper</i>
       </Box>
       <Box photoSrc={photoe} />
       <Box photoSrc={photof} />
       <Box photoSrc={photog} className="boxDistantX" />
-      <Box className="box">
-        <div className={styles.box_text_wrapper_2}>
-          <div className={styles.box_text}>
-            22.02.11
-            <br />
-            "Tenho outra vez aquela sensação de abandono.
-            <br />
-            Onde está a Alba? <br />
-            O céu está lindíssimo, há vento mas as nuvens estão altas não se
-            movem(...) <br />
-            Já não vejo nada, pouco muito pouco."
-            <br />
-            <br />
-            <br />
-            25.02.11
-            <br />
-            "Beijei-a ontem estávamos meio bêbados. <br />
-            Foi bom. Beijei-a hoje estava bastante sóbrio. Também foi bom.
-            <br />
-            Para a próxima fala menos por favor."
-            <br />
-            <br />
-            <br />
-            27.02.11
-            <br />
-            "(...)'Siempre te justificas' ela disse <br />
-            quando fiz mais uma pergunta estúpida, justificando-a. <br />
-            Ela é Mulher. É tão bom e é uma merda."
-            <br />
-          </div>
-        </div>
+      <Box className={`box ${styles.box_text}`}>
+        22.02.11
+        <br />
+        "Tenho outra vez aquela sensação de abandono.
+        <br />
+        Onde está a Alba? <br />
+        O céu está lindíssimo, há vento mas as nuvens estão altas não se
+        movem(...) <br />
+        Já não vejo nada, pouco muito pouco."
+        <br />
+        <br />
+        <br />
+        25.02.11
+        <br />
+        "Beijei-a ontem estávamos meio bêbados. <br />
+        Foi bom. Beijei-a hoje estava bastante sóbrio. Também foi bom.
+        <br />
+        Para a próxima fala menos por favor."
+        <br />
+        <br />
+        <br />
+        27.02.11
+        <br />
+        "(...)'Siempre te justificas' ela disse <br />
+        quando fiz mais uma pergunta estúpida, justificando-a. <br />
+        Ela é Mulher. É tão bom e é uma merda."
+        <br />
       </Box>
       <Box photoSrc={photoh} className="boxDistantX" />
       <Box photoSrc={photoi} />
       <Box photoSrc={photoj} />
       <Box photoSrc={photok} className="boxDistantX" />
-      <Box className="box">
-        <div className={styles.box_text_wrapper_3}>
-          <div className={styles.box_text}>
-            27.03.11
-            <br />
-            "Everyone failing to have fun."
-            <br />
-            <i>Max</i>
-            <br />
-            <br />
-            23.04.11
-            <br />
-            "(...)Muitas vezes o Max perguntava-me angustiado <br />
-            'Não temos este tipo de conversas pois não?' <br />
-            Ás vezes respondia-lhe que não <br />
-            Ás vezes respondia-lhe que não fazia mal <br />
-            ter conversas banais. <br />
-            Ás vezes, não faz."
-            <br />
-          </div>
-        </div>
+      <Box className={`box ${styles.box_text}`}>
+        27.03.11
+        <br />
+        "Everyone failing to have fun."
+        <br />
+        <i>Max</i>
+        <br />
+        <br />
+        23.04.11
+        <br />
+        "(...)Muitas vezes o Max perguntava-me angustiado <br />
+        'Não temos este tipo de conversas pois não?' <br />
+        Ás vezes respondia-lhe que não <br />
+        Ás vezes respondia-lhe que não fazia mal <br />
+        ter conversas banais. <br />
+        Ás vezes, não faz."
+        <br />
       </Box>
       <Box photoSrc={photol} />
       <Box photoSrc={photom} />
@@ -185,71 +173,63 @@ const Alba: NextPageWithLayout = () => {
       <Box photoSrc={photop} />
       <Box photoSrc={photoq} />
       <Box photoSrc={photor} className="boxDistantX" />
-      <Box className="box">
-        <div className={styles.box_text_wrapper_4}>
-          <div className={styles.box_text}>
-            "(...)I don't care, I move.
-            <br />
-            <br />
-            Maybe you're freer than I am. Good for you.
-            <br />
-            I chose a middle road between loneliness and freedom.
-            <br />
-            <br />
-            People bugged me for a long time, but that's all over now.
-            <br />
-            <br />
-            You chose total freedom but you got total loneliness.
-            <br />
-            The time comes when if you go on, you destroy yourself.
-            <br />
-            You head for destruction.
-            <br />
-            If you want to live, you stop.
-            <br />
-            My friends who stayed on the road are dead now, or
-            <br />
-            else they fell apart alcoholics, or junkies.
-            <br />
-            Because the loneliness ate them up, in the end."
-            <br />
-            <br />
-            <i>Sans Toit Ni Loi, Agnès Varda</i>
-          </div>
-        </div>
+      <Box className={`box ${styles.box_text}`}>
+        "(...)I don't care, I move.
+        <br />
+        <br />
+        Maybe you're freer than I am. Good for you.
+        <br />
+        I chose a middle road between loneliness and freedom.
+        <br />
+        <br />
+        People bugged me for a long time, but that's all over now.
+        <br />
+        <br />
+        You chose total freedom but you got total loneliness.
+        <br />
+        The time comes when if you go on, you destroy yourself.
+        <br />
+        You head for destruction.
+        <br />
+        If you want to live, you stop.
+        <br />
+        My friends who stayed on the road are dead now, or
+        <br />
+        else they fell apart alcoholics, or junkies.
+        <br />
+        Because the loneliness ate them up, in the end."
+        <br />
+        <br />
+        <i>Sans Toit Ni Loi, Agnès Varda</i>
       </Box>
       <Box photoSrc={photos} />
       <Box photoSrc={photot} />
       <Box photoSrc={photou} />
       <Box photoSrc={photov} />
       <Box photoSrc={photow} />
-      <Box className="box">
-        <div className={styles.box_text_wrapper_5}>
-          <div className={styles.box_text}>
-            06.06.11
-            <br />
-            "(...)Lembro-me de mim no terminal em Santiago deu-me lágrimas, um
-            fim, outro fim. <br />
-            Quero casa, embora não saiba muito bem o que significa isso.
-            <br />
-            Talvez mordomias. E estas esgotam-se muito depressa. Tenho a
-            certeza. <br />
-            Depois de uma semana quererei algo mais. E neste preciso momento
-            sinto que quero assentar. <br />
-            Já ontem senti isso e outros tantos dias atrás. Assentar mas não
-            parar. <br />
-            Será que serei suficientemente fiel a uma mulher mesmo que a ame?
-            <br />
-            E agora que sei que amor em viagem é ilusório. Bom mas irreal.
-            <br />
-            Porque nasce em circunstâncias que não são normais. <br />
-            Um amor de rotina talvez seja o que queira agora. Sei lá(...)"
-            <br />
-          </div>
-        </div>
+      <Box className={`box ${styles.box_text}`}>
+        06.06.11
+        <br />
+        "(...)Lembro-me de mim no terminal em Santiago deu-me lágrimas, um fim,
+        outro fim. <br />
+        Quero casa, embora não saiba muito bem o que significa isso.
+        <br />
+        Talvez mordomias. E estas esgotam-se muito depressa. Tenho a certeza.{" "}
+        <br />
+        Depois de uma semana quererei algo mais. E neste preciso momento sinto
+        que quero assentar. <br />
+        Já ontem senti isso e outros tantos dias atrás. Assentar mas não parar.{" "}
+        <br />
+        Será que serei suficientemente fiel a uma mulher mesmo que a ame?
+        <br />
+        E agora que sei que amor em viagem é ilusório. Bom mas irreal.
+        <br />
+        Porque nasce em circunstâncias que não são normais. <br />
+        Um amor de rotina talvez seja o que queira agora. Sei lá(...)"
+        <br />
       </Box>
       <Box photoSrc={photox} />
-      <Box photoSrc={photoy} className="boxDistantXX" />
+      <Box photoSrc={photoy} />
     </>
   );
 };

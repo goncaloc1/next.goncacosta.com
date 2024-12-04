581b8de02e5e2c2fce56925c658c30ff450200b9
goncaloc1 <goncacosta@gmail.com>
2024-12-04 19:59:29 +0000

    refactor: simplify `Box` component

--- components/box/box.tsx
+++ components/box/box.tsx
@@ -27,35 +27,29 @@ const isBoxPhoto = (data: BoxProps): data is BoxPhotoProps =>
   (data as BoxPhotoProps).photoSrc != null;
 
 export const Box = (props: BoxProps) => {
-  const { className = "" } = props;
+  const { className = "", legacy } = props;
 
-  return props.legacy ? (
-    <div className="cell">
+  return (
+    <div className={legacy ? "cell" : styles.box_wrapper}>
       {isBoxPhoto(props) ? (
-        <div className={`max-w-max ${className}`}>
+        <div
+          className={`${
+            legacy ? "max-w-max" : styles.box_photo_wrapper
+          } ${className}`}
+        >
           <Photo
             src={props.photoSrc}
             id={props.photoId}
             priority={props.photoPriority}
-            legacy={true}
+            legacy={legacy}
           />
         </div>
       ) : (
-        <div className={`header ${className}`}>{props.children}</div>
-      )}
-    </div>
-  ) : (
-    <div className={styles.box_wrapper}>
-      {isBoxPhoto(props) ? (
-        <div className={`${styles.box_photo_wrapper} ${className}`}>
-          <Photo
-            src={props.photoSrc}
-            id={props.photoId}
-            priority={props.photoPriority}
-          />
-        </div>
-      ) : (
-        <div className={`${styles.box_text_wrapper} ${className}`}>
+        <div
+          className={`${
+            legacy ? styles.box_text_wrapper_legacy : styles.box_text_wrapper
+          } ${className}`}
+        >
           {props.children}
         </div>
       )}
